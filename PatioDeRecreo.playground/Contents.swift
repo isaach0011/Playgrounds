d83ed388af3eae87da77c35374a470d560c125b4
isaach0011 <ihil17523@740s-pl-177.local>
2016-09-21 11:41:38 -0600

    Added playgrounds.

---
+++ PatioDeRecreo.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var thisIsNumber = 32423
+print(thisIsNumber)
+print(str + "words")
+print(str + "\(thisIsNumber)")
+print(thisIsNumber + 1232132)
+if thisIsNumber == 32423 {
+    print("Cool")
+}
+else {
+    print("Not Cool")
+}
+
+var brick = "Bricks are better"
+
+print(brick)

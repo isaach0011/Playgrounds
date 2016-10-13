c26d7eefdc30130ac884f83d29ed32493ee6c8d4
isaach0011 <ihil17523@740s-pl-177.local>
2016-10-13 11:52:56 -0600

    Added Buttons and the Date picker.

--- UIPlayground.playground/Contents.swift
+++ UIPlayground.playground/Contents.swift
@@ -2,8 +2,59 @@
 
 import UIKit
 
-var DynamicView=UIView(frame : CGRectMake(100, 200, 100, 100))
+var DynamicView = UIView(frame: CGRectMake(100, 200, 100, 100))
 DynamicView.backgroundColor=UIColor.greenColor()
-DynamicView.layer.cornerRadius=25
-DynamicView.layer.borderWidth=2
+DynamicView.layer.cornerRadius=30
+DynamicView.layer.borderWidth=10
 DynamicView
+var CoolView = UIView(frame: CGRectMake(100, 200, 100, 100))
+CoolView.backgroundColor = UIColor.clearColor()
+CoolView
+var LameView = UIView(frame: CGRectMake(100, 200, 100, 100))
+LameView.backgroundColor = UIColor.redColor()
+LameView
+
+var cool = "This is cool"
+
+var button = UIButton(frame: CGRectMake(100, 200, 200, 200))
+6+5
+var label = UILabel(frame: CGRectMake(100, 200, 300 ,100))
+
+label.text = "Duncan is Cool and good at TOL"
+label.backgroundColor=UIColor.blueColor()
+label.layer.masksToBounds = true
+label.layer.cornerRadius = 20
+label.textAlignment = NSTextAlignment.Center
+label
+
+
+var coollabel = UILabel(frame: CGRectMake(100, 200, 400, 100))
+
+coollabel.text = "I am very tired..."
+coollabel.backgroundColor=UIColor.brownColor()
+coollabel.layer.masksToBounds = true
+coollabel.layer.cornerRadius = 30
+coollabel.textAlignment = .Center
+coollabel.font = UIFont(name: "MarkerFelt-Thin", size: 45)
+coollabel
+var coolButton = UIButton(frame: CGRectMake(100, 200, 500, 100))
+
+
+coolButton.backgroundColor = UIColor.whiteColor()
+coolButton.layer.masksToBounds = true
+coolButton.layer.cornerRadius = 40
+coolButton
+var lameButton = UIButton(frame: CGRectMake(100, 200, 200, 100))
+
+lameButton.backgroundColor = UIColor.lightGrayColor()
+lameButton.layer.masksToBounds = true
+lameButton.layer.cornerRadius = 50
+lameButton
+let view = UIView()
+let datePicker = UIDatePicker()
+datePicker.datePickerMode = .Date
+datePicker.backgroundColor = UIColor.whiteColor()
+
+datePicker
+
+
